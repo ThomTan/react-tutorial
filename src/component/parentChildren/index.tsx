49d6caa32efae2f313139ce585ec796897cefa4c
tqm <2631260229@qq.com>
2019-10-09 20:39:09 +0800

    update

--- src/component/parentChildren/index.tsx
+++ src/component/parentChildren/index.tsx
@@ -1,54 +1,52 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Text, Children } from './children'
 
 interface OwnState {
-  list: Text[];
-}
-
-interface Text {
-  id: number;
-  txt: string;
+  list: Text[]
 }
 
 export default class ParentChildren extends Component<{}, OwnState> {
-  state: OwnState;
+  state: OwnState
 
   constructor(props: {}) {
-    super(props);
+    super(props)
     this.state = {
       list: []
-    };
-    this.txtAddDom = React.createRef();
+    }
+    this.txtAddDom = React.createRef()
   }
 
-  private randomNumber = (): number => Math.round(Math.random() * 100000);
+  private randomNumber = (): number => Math.round(Math.random() * 100000)
 
-  private txtAddDom: React.RefObject<HTMLInputElement>;
+  private txtAddDom: React.RefObject<HTMLInputElement>
 
-  addTxtClick(): void {
-    const txt = this.txtAddDom.current ? this.txtAddDom.current.value : null;
-    if (!txt) return;
-    const obj: Text = { id: this.randomNumber(), txt: txt };
+  private addTxtClick(): void {
+    const txt = this.txtAddDom.current ? this.txtAddDom.current.value : null
+    if (!txt) return
+    const obj: Text = { id: this.randomNumber(), txt: txt }
     this.setState({
       list: this.state.list.concat(obj)
-    });
+    })
+  }
+
+  private addTxtKeyDown(keyCode: number): void {
+    if (keyCode !== 13) return
+    this.addTxtClick()
   }
 
   render() {
     return (
       <div>
-        <input type="text" ref={this.txtAddDom} />
+        <input
+          type="text"
+          ref={this.txtAddDom}
+          onKeyDown={e => this.addTxtKeyDown(e.keyCode)}
+        />
         <button id="btnAdd" onClick={() => this.addTxtClick()}>
           添加
         </button>
-
-        <div>
-          <ul>
-            {this.state.list.map(g => (
-              <li key={g.id}>{g.txt}</li>
-            ))}
-          </ul>
-        </div>
+        <Children list={this.state.list} />
       </div>
-    );
+    )
   }
 }
