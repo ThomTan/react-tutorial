4330667676f7c3849187c671ecc4c0b0270162b7
tqm <2631260229@qq.com>
2019-10-21 09:35:18 +0800

    createContext Store

---
+++ src/components/Counter/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import { CounterConsumer } from '../../counterStore'
+
+export default class Counter extends Component {
+  render() {
+    return (
+      <CounterConsumer>
+        {({ count }) => {
+          return <span>{count}</span>
+        }}
+      </CounterConsumer>
+    )
+  }
+}
